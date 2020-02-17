765b600f6091382f29cc7b5ee68874ff4ae9628b
Leseratte10 <Leseratte10@users.noreply.github.com>
2020-02-17 17:44:38 +0100

    Update d2x-beta.bat

--- data/d2x-beta.bat
+++ data/d2x-beta.bat
@@ -9,34 +9,34 @@ set d2x-beta-rev=${MAJOR_VER}-${MINOR_VER}
 ::This info is used to sign *00.app file for cIOS detection, everything after "=" but before the first "-" is considered the version # and it must be a number! Good examples are "5-beta1-test1" or "5" or "999". Bad examples are "5beta1" or "five-beta1" or "-5-beta1"
 ::everything after the first "-" is considered the version string (limited by 16 chars) and it only recognizes letters, numbers and + - _ ( ) [ ]
 
-set cIOSFamilyName=d2x
-::Only change this if building a cIOS that isn't a "d2x" branded cIOS. This string is limited by 16 chars and it only recognizes letters, numbers and + - _ ( ) [ ]
+set cIOSFamilyName=d2xl
+::Only change this if building a cIOS that isn't a "d2xl" branded cIOS. This string is limited by 16 chars and it only recognizes letters, numbers and + - _ ( ) [ ]
 
 
 if "%wadname%"=="" goto:endofd2xbat
 
 ::------------------------Section to update (below)--------------------
 ::here you should enter the new beta cIOS hashes (under the OLD non-beta "wadname")
-if /i "%wadname:~0,17%" EQU "cIOS249[37]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[37]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[38]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[38]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[53]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[53]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[55]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[55]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[56]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[56]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[57]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[57]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[58]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[58]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[60]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[60]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[70]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[70]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS249[80]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
-if /i "%wadname:~0,17%" EQU "cIOS250[80]-d2x-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[37]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[37]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[38]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[38]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[53]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[53]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[55]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[55]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[56]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[56]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[57]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[57]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[58]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[58]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[60]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[60]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[70]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[70]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS249[80]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
+if /i "%wadname:~0,17%" EQU "cIOS250[80]-d2xl-v" set md5=ac0ffee1eddad70defec81005e570015
 ::------------------------Section to update (above)--------------------
 
 ::----------Additional Instructions---------------
