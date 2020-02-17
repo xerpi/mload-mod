4e7081ef71a7ff149602b8185b8410ef94dd212a
Leseratte10 <Leseratte10@users.noreply.github.com>
2020-02-17 17:43:18 +0100

    Change git version identifier
    An equal sign in the cIOS version isn't allowed, replacing it with a plus sign instead

--- dist.sh
+++ dist.sh
@@ -13,4 +13,4 @@ g++ main.cpp -o stripios
 cp stripios ../source/stripios
 cd ..
 dos2unix ./maked2x.sh
-./maked2x.sh 999 git=$(git rev-parse --short HEAD)
+./maked2x.sh 999 git+$(git rev-parse --short HEAD)
